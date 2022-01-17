17f2ed2cf128086e7907af20617f7a8a2e019793
sk8marlon <marlonsgb@hotmail.com>
2022-01-17 10:43:25 -0300

    ui restrictions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @Suser.id
+      session[:user_id] = @user.id
       flash[:notice] = "welcome to the alpha blog #{@user.username}, you have successfully singup"
       redirect_to articles_path
     else
