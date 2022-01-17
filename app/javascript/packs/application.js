7b7096d8cb61bebc6d75f7f4416c85d37eca1287
sk8marlon <marlonsgb@hotmail.com>
2022-01-17 15:52:56 -0300

    delete user *java not working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,9 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("bootstrap")
-require("bootstrap-sprockets")
-require("jquery")
-require("jquery_ujs")
-require("jquery-ui")
+import 'bootstrap';
+import '../stylesheets/application'
+
 require("popper")
 require("@rails/ujs").start()
 require("turbolinks").start()
